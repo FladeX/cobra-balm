a9aa1b8f131c84edcbb11d5294abbcca7a3aaa6b
FladeX <FladeX@yandex.ru>
2014-12-21 16:18:06 +0300

    add header

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function() {
+	gulp.src('assets/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['less']);
