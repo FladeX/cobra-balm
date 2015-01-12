0d0bede6d2a0bc51cb09ac0490c57849379ad1cd
FladeX <FladeX@yandex.ru>
2015-01-12 18:18:19 +0300

    add js for smooth scroll

--- assets/coffee/main.coffee
+++ assets/coffee/main.coffee
@@ -5,3 +5,13 @@ $(document).ready ->
 			$menu.addClass('b-header_mode_fixed')
 		else if $(this).scrollTop() <= 60 and $menu.hasClass('b-header_mode_fixed')
 			$menu.removeClass('b-header_mode_fixed')
+	$('a[href^="#"]').on 'click', (e) ->
+		e.preventDefault()
+
+		target = this.hash
+		$target = $(target)
+
+		$('html, body').stop().animate
+			'scrollTop': $target.offset().top
+		, 900, 'swing', ->
+			window.location.hash = target
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,23 @@
   $(document).ready(function() {
     var $menu;
     $menu = $('#header');
-    return $(window).scroll(function() {
+    $(window).scroll(function() {
       if ($(this).scrollTop() > 60) {
         return $menu.addClass('b-header_mode_fixed');
       } else if ($(this).scrollTop() <= 60 && $menu.hasClass('b-header_mode_fixed')) {
         return $menu.removeClass('b-header_mode_fixed');
       }
+    });
+    return $('a[href^="#"]').on('click', function(e) {
+      var $target, target;
+      e.preventDefault();
+      target = this.hash;
+      $target = $(target);
+      return $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 900, 'swing', function() {
+        return window.location.hash = target;
+      });
     });
   });
 
