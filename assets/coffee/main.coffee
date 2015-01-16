73ef0e28375f96fbb03cd6c03a06369057b49d95
FladeX <FladeX@yandex.ru>
2015-01-16 22:06:00 +0300

    add js for show and hide popup

--- assets/coffee/main.coffee
+++ assets/coffee/main.coffee
@@ -12,7 +12,27 @@ $(document).ready ->
 		target = this.hash
 		$target = $(target)
 
+		if target == '#order'
+			offsetTop = $(window).scrollTop()
+			$('html, body').css
+				'height': '100%'
+				'overflow': 'hidden'
+			.scrollTop(offsetTop)
+			$('.b-popup').show()
 		$('html, body').stop().animate
 			'scrollTop': $target.offset().top
 		, 900, 'swing', ->
 			window.location.hash = target
+	$('.b-popup__close').on 'click', ->
+		$('html, body').css
+			'height': 'auto'
+			'overflow': 'auto'
+		$('.b-popup').css
+			display: 'none'
+	$('.b-intro__order-button, .b-components__order-button').on 'click', ->
+		offsetTop = $(window).scrollTop()
+		$('html, body').css
+			'height': '100%'
+			'overflow': 'hidden'
+		.scrollTop(offsetTop)
+		$('.b-popup').show()
--- assets/js/main.js
+++ assets/js/main.js
@@ -10,16 +10,42 @@
         return $menu.removeClass('b-header_mode_fixed');
       }
     });
-    return $('a[href^="#"]').on('click', function(e) {
-      var $target, target;
+    $('a[href^="#"]').on('click', function(e) {
+      var $target, offsetTop, target;
       e.preventDefault();
       target = this.hash;
       $target = $(target);
+      if (target === '#order') {
+        offsetTop = $(window).scrollTop();
+        $('html, body').css({
+          'height': '100%',
+          'overflow': 'hidden'
+        }).scrollTop(offsetTop);
+        $('.b-popup').show();
+      }
       return $('html, body').stop().animate({
         'scrollTop': $target.offset().top
       }, 900, 'swing', function() {
         return window.location.hash = target;
       });
+    });
+    $('.b-popup__close').on('click', function() {
+      $('html, body').css({
+        'height': 'auto',
+        'overflow': 'auto'
+      });
+      return $('.b-popup').css({
+        display: 'none'
+      });
+    });
+    return $('.b-intro__order-button, .b-components__order-button').on('click', function() {
+      var offsetTop;
+      offsetTop = $(window).scrollTop();
+      $('html, body').css({
+        'height': '100%',
+        'overflow': 'hidden'
+      }).scrollTop(offsetTop);
+      return $('.b-popup').show();
     });
   });
 
